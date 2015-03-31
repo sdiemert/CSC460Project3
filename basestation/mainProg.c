adcbf03ccee1bdca3cd116ef8091ea2eecdd2df9
Simon Diemert <simon.diemert@gmail.com>
2015-03-30 19:26:35 -0700

    Added logic for hits and stuns

--- basestation/mainProg.c
+++ basestation/mainProg.c
@@ -25,6 +25,9 @@ typedef struct{
     uint8_t joystick_port; //the port the joystick is located on. 
     TEAM team;
     uint8_t status;
+    uint8_t stun_duration; // Stun duration in MS
+    uint8_t stun_elapsed; 
+    uint8_t hits;
 } roomba_t; 
 
 roomba_t roombas[4];
@@ -57,11 +60,10 @@ void updateRoomba(){
     uint8_t joystick_x = 0;
     uint8_t joystick_y = 0;    
     uint8_t button = 0;
-
     char output[20]; 
 
     while(1) {
-        for(current_roomba = 0; current_roomba < 2; current_roomba++) {
+        for(current_roomba = 0; current_roomba < 4; current_roomba++) {
             //Radio_Flush();
 
             PORTB |= 1 << 5; 
@@ -75,9 +77,23 @@ void updateRoomba(){
 
             // TODO Button Press
 
-            // TODO Update State
+            // Handle stunned zombies here. Turn their state back to normal after a certain amount of time.
+            if(roombas[current_roomba].status == (uint8_t)STUNNED){
+                if(roombas[current_roomba].stun_elapsed >= roombas[current_roomba].stun_duration){
+                    //don't need to stun anymore.
+                    roombas[current_roomba].status = (uint8_t)NORMAL; 
+                    roombas[current_roomba].stun_elapsed = 0;
+                    sendPacket(current_roomba, joystick_x, joystick_y, button);    
+                }else{
+                    //increment the stun
+                    roombas[current_roomba].stun_elapsed += 1;
+                    sendPacket(current_roomba, 127, 127, 0);
+                }
+            }else{
+                //not stunned, just continue as is.
+                sendPacket(current_roomba, joystick_x, joystick_y, button);
+            }
 
-            sendPacket(current_roomba, joystick_x, joystick_y, button);
 
             PORTB &= ~(1 << 5); 
             Task_Next();
@@ -90,6 +106,7 @@ void manageReceive(){
     int16_t x; 
     char output[50];
     uint8_t result;
+    pf_game_t roomba_game_state;
     while(1){
         Service_Subscribe(rx_service, &x);
         PORTB |= (1 << 4);
@@ -99,8 +116,39 @@ void manageReceive(){
             result = Radio_Receive(&rx_packet);            
         }
 
-        sprintf(output, "packet from: %d \n\r",rx_packet.payload.game.game_player_id);
+        sprintf(output, "from: %d, enemy: %d, hit: %d, team: %d, state: %d\n\r",
+                rx_packet.payload.game.game_player_id, 
+                rx_packet.payload.game.game_enemy_id, 
+                rx_packet.payload.game.game_hit_flag,
+                rx_packet.payload.game.game_team,
+                rx_packet.payload.game.game_state
+        );
         trace_uart_putstr(output);
+
+        roomba_game_state = rx_packet.payload.game;
+
+        // Handle a hit
+        if(roomba_game_state.game_hit_flag == 1) {
+            uint8_t player_hit = roomba_game_state.game_player_id;
+            uint8_t player_shooting = roomba_game_state.game_enemy_id;
+
+            // TODO Error Handling
+
+            // If the player hit is a human and the player shooting is a zombie, the human should lose his/her shield
+            if(roombas[player_hit].team == HUMAN && roombas[player_hit].status == (uint8_t)SHIELDED && roombas[player_shooting].team == ZOMBIE) {
+                roombas[player_hit].status = (uint8_t)SHIELDLESS;
+            } else if(roombas[player_hit].team == ZOMBIE && roombas[player_shooting].team == HUMAN) {
+                roombas[player_hit].status = (uint8_t)STUNNED;
+                roombas[player_hit].stun_duration += 6;
+                roombas[player_hit].stun_elapsed = 0;
+            } else if(roombas[player_hit].team == HUMAN  && roombas[player_hit].status == (uint8_t)SHIELDLESS && roombas[player_shooting].team == ZOMBIE) {
+                roombas[player_hit].team = ZOMBIE;
+                roombas[player_hit].status = (uint8_t)NORMAL;
+            }
+
+            // Increment the player shooting's hit counter
+            roombas[player_shooting].hits += 1;
+        }
 
         PORTB &= ~(1 << 4);
 
@@ -114,22 +162,30 @@ void init_game() {
     roombas[0].id = 0;
     roombas[0].joystick_port = 0;
     roombas[0].team = ZOMBIE;
-    roombas[0].status = (uint8_t)NORMAL;    
+    roombas[0].status = (uint8_t)NORMAL; 
+    roombas[0].stun_duration = 0;
+    roombas[0].hits = 0;
 
     roombas[1].id = 1;
     roombas[1].joystick_port = 4;
     roombas[1].team = HUMAN;
     roombas[1].status = (uint8_t)SHIELDED;
+    roombas[1].stun_duration = 0;
+    roombas[1].hits = 0;    
 
     roombas[2].id = 2;
     roombas[2].joystick_port = 8;
     roombas[2].team = HUMAN;
-    roombas[2].status = (uint8_t)SHIELDED;    
+    roombas[2].status = (uint8_t)SHIELDED; 
+    roombas[2].stun_duration = 0;
+    roombas[2].hits = 0;
 
     roombas[3].id = 3;
     roombas[3].joystick_port = 12;
     roombas[3].team = HUMAN;
-    roombas[3].status = (uint8_t)SHIELDED;  
+    roombas[3].status = (uint8_t)SHIELDED;
+    roombas[3].stun_duration = 0;
+    roombas[3].hits = 0;
 }
 
 
