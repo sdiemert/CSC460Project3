536403786be029cb84a641eaf942709e1cbf9faf
Stymphalian <saturnslight@gmail.com>
2015-03-31 17:37:01 -0700

    jordanpaul roomba code

--- jordanpaul/jordanpaul.c
+++ jordanpaul/jordanpaul.c
@@ -1,3 +1,247 @@
+#if 0
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "rtos/os.h"
+#include "rtos/kernel.h"
+#include "radio/radio.h"
+#include "game.h"
+#include "trace_uart/trace_uart.h"
+#include "joystick/joystick.h"
+
+radiopacket_t tx_packet;
+radiopacket_t rx_packet;
+pf_game_t game_packet;
+
+SERVICE * rx_service;
+
+uint8_t basestation_address[5] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+uint8_t zombie_count = 1;
+
+void radio_rxhandler(uint8_t);
+void ir_rxhandler(){}
+
+//a struct to hold all the information for a player
+typedef struct{
+    uint8_t id;
+    uint8_t joystick_port; //the port the joystick is located on.
+    TEAM team;
+    uint8_t status;
+    uint8_t stun_duration; // Stun duration in MS
+    uint8_t stun_elapsed;
+    uint8_t hits;
+} roomba_t;
+
+roomba_t roombas[4];
+
+// End game task
+void end_game() {
+    while(1) {
+        // TODO End game logic
+    }
+}
+
+void radio_rxhandler(uint8_t val){
+    PORTB ^= 1 << 6;
+    // PORTB ^= 1 << 7;
+    Service_Publish(rx_service, 1);
+}
+
+void sendPacket(uint8_t id, uint8_t x, uint8_t y, uint8_t b){
+
+    Radio_Set_Tx_Addr(ROOMBA_ADDRESSES[id]);
+
+    game_packet.velocity_x = x;
+    game_packet.velocity_y = y;
+    game_packet.button ^= game_packet.button;
+    game_packet.game_player_id = id;
+    game_packet.game_team = (uint8_t)roombas[id].team;
+    game_packet.game_state = (uint8_t)roombas[id].status;
+
+    memcpy(tx_packet.payload.game.sender_address, basestation_address, RADIO_ADDRESS_LENGTH);
+    tx_packet.payload.game = game_packet;
+
+    Radio_Transmit(&tx_packet, RADIO_WAIT_FOR_TX);
+    PORTB ^= 1 << 7;
+}
+
+void updateRoomba(){
+    uint8_t current_roomba;
+    uint8_t joystick_x = 0;
+    uint8_t joystick_y = 0;
+    uint8_t button = 0;
+    char output[20];
+
+    while(1) {
+        for(current_roomba = 0; current_roomba < 4; current_roomba++) {
+            //Radio_Flush();
+
+            PORTB |= 1 << 5;
+
+            joystick_x = read_analog(roombas[current_roomba].joystick_port);
+            joystick_y = read_analog(roombas[current_roomba].joystick_port+1);
+
+            if(current_roomba==1){
+               sprintf(output, "%d: (%d,%d)\n\r",current_roomba,joystick_x, joystick_y) ;
+            }
+            trace_uart_putstr(output);
+
+            // TODO Button Press
+
+            // Handle stunned zombies here. Turn their state back to normal after a certain amount of time.
+            if(roombas[current_roomba].status == (uint8_t)STUNNED){
+                if(roombas[current_roomba].stun_elapsed >= roombas[current_roomba].stun_duration){
+                    //don't need to stun anymore.
+                    roombas[current_roomba].status = (uint8_t)NORMAL;
+                    roombas[current_roomba].stun_elapsed = 0;
+                    sendPacket(current_roomba, joystick_x, joystick_y, button);
+                }else{
+                    //increment the stun
+                    roombas[current_roomba].stun_elapsed += 1;
+                    sendPacket(current_roomba, 127, 127, 0);
+                }
+            }else{
+                //not stunned, just continue as is.
+                sendPacket(current_roomba, joystick_x, joystick_y, button);
+            }
+
+
+            PORTB &= ~(1 << 5);
+            Task_Next();
+        }
+    }
+}
+
+void manageReceive(){
+
+    int16_t x;
+    char output[50];
+    uint8_t result;
+    pf_game_t roomba_game_state;
+    while(1){
+        Service_Subscribe(rx_service, &x);
+        PORTB |= (1 << 4);
+        result = Radio_Receive(&rx_packet);
+
+        while(result == RADIO_RX_MORE_PACKETS){
+            result = Radio_Receive(&rx_packet);
+        }
+
+        sprintf(output, "from: %d, enemy: %d, hit: %d, team: %d, state: %d\n\r",
+                rx_packet.payload.game.game_player_id,
+                rx_packet.payload.game.game_enemy_id,
+                rx_packet.payload.game.game_hit_flag,
+                rx_packet.payload.game.game_team,
+                rx_packet.payload.game.game_state
+        );
+
+        //trace_uart_putstr(output);
+        //roomba_game_state = rx_packet.payload.game;
+
+        // Handle a hit
+        if(roomba_game_state.game_hit_flag == 1) {
+            uint8_t player_hit = roomba_game_state.game_player_id;
+            uint8_t player_shooting = roomba_game_state.game_enemy_id;
+
+            // TODO Error Handling
+
+            // If the player hit is a human and the player shooting is a zombie, the human should lose his/her shield
+            if(roombas[player_hit].team == HUMAN && roombas[player_hit].status == (uint8_t)SHIELDED && roombas[player_shooting].team == ZOMBIE) {
+                roombas[player_hit].status = (uint8_t)SHIELDLESS;
+            } else if(roombas[player_hit].team == ZOMBIE && roombas[player_shooting].team == HUMAN) {
+                roombas[player_hit].status = (uint8_t)STUNNED;
+                roombas[player_hit].stun_duration += 6;
+                roombas[player_hit].stun_elapsed = 0;
+            } else if(roombas[player_hit].team == HUMAN  && roombas[player_hit].status == (uint8_t)SHIELDLESS && roombas[player_shooting].team == ZOMBIE) {
+                roombas[player_hit].team = ZOMBIE;
+                roombas[player_hit].status = (uint8_t)NORMAL;
+                zombie_count ++;
+            }
+
+            // Increment the player shooting's hit counter
+            roombas[player_shooting].hits += 1;
+        }
+
+        // Check if we should end the game
+        if(zombie_count == 4) {
+            Task_Create_System(end_game, 0);
+        }
+
+        PORTB &= ~(1 << 4);
+
+        Task_Next();
+    }
+
+}
+
+void init_game() {
+    // Setup Roombas
+    roombas[0].id = 0;
+    roombas[0].joystick_port = 0;
+    roombas[0].team = ZOMBIE;
+    roombas[0].status = (uint8_t)NORMAL;
+    roombas[0].stun_duration = 0;
+    roombas[0].hits = 0;
+
+    roombas[1].id = 1;
+    roombas[1].joystick_port = 4;
+    roombas[1].team = HUMAN;
+    roombas[1].status = (uint8_t)SHIELDED;
+    roombas[1].stun_duration = 0;
+    roombas[1].hits = 0;
+
+    roombas[2].id = 2;
+    roombas[2].joystick_port = 8;
+    roombas[2].team = HUMAN;
+    roombas[2].status = (uint8_t)SHIELDED;
+    roombas[2].stun_duration = 0;
+    roombas[2].hits = 0;
+
+    roombas[3].id = 3;
+    roombas[3].joystick_port = 12;
+    roombas[3].team = HUMAN;
+    roombas[3].status = (uint8_t)SHIELDED;
+    roombas[3].stun_duration = 0;
+    roombas[3].hits = 0;
+}
+
+
+int r_main(){
+
+    //set up LED
+    DDRB |= 1 << 7;
+    DDRB |= 1 << 6;
+    DDRB |= 1 << 5;
+    DDRB |= 1 << 4;
+
+    /* Set up radio */
+    DDRL |= (1 << PL2);
+    PORTL &= ~(1 << PL2);
+    _delay_ms(500);
+    PORTL |= (1 << PL2);
+    _delay_ms(500);
+    Radio_Init();
+    Radio_Configure_Rx(RADIO_PIPE_0, basestation_address, ENABLE);
+    Radio_Configure(RADIO_1MBPS, RADIO_HIGHEST_POWER);
+
+
+    rx_service = Service_Init();
+
+    trace_uart_init();
+
+    /*setup joystick controllers*/
+    setup_controllers();
+    init_game();
+
+    /*Create our tasks for the base station*/
+    Task_Create_Periodic(updateRoomba, 1, 20, 15, 250);
+    Task_Create_RR(manageReceive, 0);
+
+    return 0;
+}
+
+#else
+
 #include <avr/io.h>
 #include "rtos/os.h"
 #include "rtos/timer.h"
@@ -14,15 +258,12 @@
 #define LED_RX_PIN    PB4
 #define LED_TX_PIN    PB5
 #define LED_IR_PIN    PB6
-#define LED_GREEN_PIN PB7
+
 // TODO: Change this to a proper pin number
-#define LED_RED_PIN   PB7
-
-#define ZOMBIE_STOP 0
-#define ZOMBIE_GO 1
-
-// ONLY TEMPORARY
-uint8_t ir_count = 0;
+#define TEAM_DDR            DDRC
+#define TEAM_PORT           PORTC
+#define TEAM_HUMAN_PIN      PC5
+#define TEAM_ZOMBIE_PIN     PC4
 
 SERVICE* radio_receive_service;
 SERVICE* ir_receive_service;
@@ -48,9 +289,7 @@ typedef struct _model_t {
     int16_t vy;
     uint8_t button;
 
-
     // control the if the periodic tasks run. 0 for off ,1 for on
-    uint8_t blink_led_flag;
     uint8_t autonomous_flag;
 
     // function pointers to how we want to handle stuff
@@ -80,44 +319,38 @@ void ir_rxhandler() {
 
 void handleRoombaInput(pf_game_t* game)
 {
-    // map the given input into stuff.
+    int16_t x_value = (((game->velocity_x*5)/256) - 2)*250;
+    int16_t y_value = (((game->velocity_y*5)/256) - 2)*250;
 
-	int16_t vx = (game->velocity_x/(255/9) - 4)*50;
-	int16_t vy = (game->velocity_y/(255/9) - 4)*50;
-
-    model.old_vx     =  model.vx;
-    model.old_vy     =  model.vy;
-    model.old_button =  model.button;
-    model.vx     =  vx;
-    model.vy     =  vy;
-    model.button =  game->button;
-
-	if( vy == 0){
-		if( vx > 0){
-            vy = 200;
-			vx = 1;
-		}else if( vx < 0){
-            vy = 200;
-			vx = -1;
-		}
-	}
-
-    // pass of to our controller
-	Roomba_Drive(vy,vx);
+    if( x_value == 0 && y_value == 0){
+        Roomba_Drive(0,0x8000);
+    } else if( x_value == 0){
+        y_value = -y_value;
+        Roomba_Drive(y_value,0x8000);
+    } else if( y_value == 0){
+      // turn in place
+      uint16_t deg = -1;
+      if( x_value < 0 ){
+        deg = 1;
+      }
+      Roomba_Drive(500,deg);
+    }else{
+      x_value = -x_value;
+      y_value = -y_value;
+      Roomba_Drive(y_value,x_value);
+    }
 
     // fire every 5th packet
-    if( ir_count == 5){
+    if( game->button ){
         IR_transmit(model.player_id);
-        ir_count = 0;
     }
-    ir_count++;
 }
 
 void handleStateInput(pf_game_t* game){
     model.old_team = model.team;
     model.old_state = model.team;
-    model.team = game->game_team;
-    model.state = game->game_state;
+    // model.team = game->game_team;
+    // model.state = game->game_state;
 
     if( model.old_team != model.team){
         // we have changed teamd
@@ -129,6 +362,9 @@ void handleStateInput(pf_game_t* game){
             // we went from zombie to human. SHOULD NOT HAPPEN
             OS_Abort();
         }
+
+        TEAM_PORT &= ~( 1<< TEAM_HUMAN_PIN);
+        TEAM_PORT &= ~( 1<< TEAM_ZOMBIE_PIN);
 
     }else{
         // we have not changed teams
@@ -149,25 +385,24 @@ void handleStateInput(pf_game_t* game){
         }
     }
 
+    // TODO: turn of the team leds
     if( model.team == HUMAN){
         // set the green led
         if(model.state == SHIELDED){
             // solid green
-            model.blink_led_flag = 0;
-            LED_PORT |= (1 << LED_GREEN_PIN);
+
         }else{
             // blinking green
-            model.blink_led_flag = 1;
+
         }
     }else{
         // set the red led
         if( model.state == NORMAL){
             // solid red
-            model.blink_led_flag = 0;
-            LED_PORT |= (1 << LED_RED_PIN);
+
         }else{
             // blinking red
-            model.blink_led_flag = 1;
+
         }
     }
 }
@@ -215,6 +450,7 @@ void rr_roomba_controller() {
 		Service_Subscribe(radio_receive_service,&value);
 
 		do {
+
 			result = Radio_Receive(&packet);
 
 			if(result == RADIO_RX_SUCCESS || result == RADIO_RX_MORE_PACKETS) {
@@ -222,14 +458,14 @@ void rr_roomba_controller() {
 				{
                     // wake up our roomba controller?
 					handleRoombaInput(&packet.payload.game);
-					handleStateInput(&packet.payload.game);
+					//handleStateInput(&packet.payload.game);
 				}
 			}
 
 		} while (result == RADIO_RX_MORE_PACKETS);
 
         // POTENTIAL STRAT, don' send packet back until as late as possible
-		send_back_packet();
+		//send_back_packet();
 	}
 }
 
@@ -247,38 +483,26 @@ void rr_ir_controller()
 
 void p_blink_led()
 {
+    uint8_t mask;
     for(;;)
     {
-        while(model.blink_led_flag)
-        {
-            if( model.team == ZOMBIE){
-                LED_PORT ^= (1 << LED_RED_PIN);
+        if( model.team == ZOMBIE){
+            if( model.state == NORMAL){
+                TEAM_PORT |= (1<< TEAM_ZOMBIE_PIN);
             }else{
-                LED_PORT ^= (1 << LED_GREEN_PIN);
+                TEAM_PORT ^= ( 1 <<TEAM_ZOMBIE_PIN);
             }
-            Task_Next();
+        }else{
+            if( model.state == SHIELDED){
+                TEAM_PORT |= (1 << TEAM_HUMAN_PIN);
+            }else{
+                TEAM_PORT ^= (1<< TEAM_HUMAN_PIN);
+            }
         }
+
         Task_Next();
     }
 }
-
-void p_autonomous()
-{
-    for(;;)
-    {
-        while(model.autonomous_flag)
-        {
-            // do whatever
-            // read roomba sensors..
-            // do drive command
-            // fire ir gun
-
-            Task_Next();
-        }
-        Task_Next();
-    }
-}
-
 
 // Note that this will delay by 1 second (200 ticks)
 void power_cycle_radio()
@@ -297,13 +521,15 @@ void setup_leds()
     LED_DDR |= (1<<LED_RX_PIN);
     LED_DDR |= (1<<LED_TX_PIN);
     LED_DDR |= (1<<LED_IR_PIN);
-    LED_DDR |= (1<<LED_GREEN_PIN);
-    LED_DDR |= (1<<LED_RED_PIN);
     LED_PORT &= ~(1<<LED_RX_PIN);
     LED_PORT &= ~(1<<LED_TX_PIN);
     LED_PORT &= ~(1<<LED_IR_PIN);
-    LED_PORT &= ~(1<<LED_GREEN_PIN);
-    LED_PORT &= ~(1<<LED_RED_PIN);
+
+
+    TEAM_DDR |= (1<<TEAM_HUMAN_PIN);
+    TEAM_DDR |= (1<<TEAM_ZOMBIE_PIN);
+    TEAM_PORT &= ~(1<<TEAM_HUMAN_PIN);
+    TEAM_PORT &= ~(1<<TEAM_ZOMBIE_PIN);
 }
 
 void init_model(Model_t* model)
@@ -320,10 +546,8 @@ void init_model(Model_t* model)
     model->vx = 0;
     model->vy = 0;
     model->button = 0;
-
-    model->blink_led_flag = 0;
-    model->autonomous_flag= 0;
 }
+
 
 int r_main(void)
 {
@@ -347,9 +571,10 @@ int r_main(void)
 
     // periodic tasks to control blinking the led + controlling a stunned zombie
     // they will be flag gaurded such that they only run once they are allowed.
-    Task_Create_Periodic(p_blink_led ,0,50,3,250);
-    Task_Create_Periodic(p_autonomous,0,100,50,251);
+    Task_Create_Periodic(p_blink_led ,0,10,3,250);
 
 	Task_Terminate();
 	return 0 ;
 }
+
+#endif
